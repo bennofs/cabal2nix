127bd1547252038864ce3d7f6c1bb31d23393c9d
Peter Simons <simons@cryp.to>
2013-07-08 14:18:12 +0200

    Cabal2Nix/PostProcess.hs: libcrypt is part of glibc

--- src/Cabal2Nix/PostProcess.hs
+++ src/Cabal2Nix/PostProcess.hs
@@ -81,6 +81,7 @@ postProcess deriv@(MkDerivation {..})
   | pname == "terminfo"         = deriv { extraLibs = "ncurses":extraLibs }
   | pname == "threadscope"      = deriv { configureFlags = "--ghc-options=-rtsopts":configureFlags }
   | pname == "unix-time"        = deriv { phaseOverrides = unixTimeConfigureFlags }
+  | pname == "Unixutils"        = deriv { extraLibs = delete "crypt" extraLibs }
   | pname == "vacuum"           = deriv { extraLibs = "ghcPaths":extraLibs }
   | pname == "wxc"              = deriv { extraLibs = "wxGTK":"mesa":"libX11":extraLibs, phaseOverrides = wxcPostInstall }
   | pname == "wxcore"           = deriv { extraLibs = "wxGTK":"mesa":"libX11":extraLibs }
